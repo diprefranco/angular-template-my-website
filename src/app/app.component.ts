ae826dde997d10401c7ab9cba69d1678a50aa48f
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-10-16 10:40:10 -0300

    Full copied from repo: diprefranco.github.io, branch: dev-layout-template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from "./layout/footer/footer.component";
+import { HeroComponent } from "./layout/hero/hero.component";
+import { SampleImagesComponent } from "./layout/sample-images/sample-images.component";
+import { CardsComponent } from './layout/cards/cards.component';
+import { MoreInformationComponent } from "./layout/more-information/more-information.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    SampleImagesComponent,
+    CardsComponent,
+    MoreInformationComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-template-my-website';
 }
